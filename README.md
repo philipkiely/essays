851ee8bfcbb1164a69ca81fc1d21ba3d8a11723e
Philip Kiely <kielyphi@grinnell.edu>
2019-06-24 06:16:55 -0400

    autocommit outside-posts.md

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# blog
+# Essays
 A technical and business blog focusing on high-value content. Repository may include files for CMS.
 
 All code in this blog including CMS is MIT licensed. The articles themselves are the sole property of the author.
---
+++ theme/script.js
@@ -0,0 +1,32 @@
+function setYear() {
+    document.getElementById("year").innerHTML = (new Date()).getFullYear();
+}
+
+window.onload = setYear();
+
+const sleep = (milliseconds) => {
+  return new Promise(resolve => setTimeout(resolve, milliseconds))
+}
+
+
+function emailListSignup() {
+    var email = document.getElementById("get-email").value
+    if (email.indexOf("@") != -1 && email.indexOf(".") != -1) {
+        var xhr = new XMLHttpRequest();
+        xhr.open("POST", "https://2opt1v52m0.execute-api.us-east-1.amazonaws.com/default/EmailListNewMember", true);
+        xhr.setRequestHeader('Content-Type', 'application/json');
+        xhr.send(JSON.stringify({
+            email: email
+        }));
+        document.getElementById("get-email").value = "";
+        document.getElementById("invalid-email").hidden = true;
+        document.getElementById("valid-email").hidden = false;
+        sleep(5000).then(() => {
+            document.getElementById("valid-email").hidden = true;
+        })
+    }
+    else {
+        document.getElementById("invalid-email").hidden = false;
+    }
+    
+}
